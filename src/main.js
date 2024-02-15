3a0c2123319a21206a999ba9ebd24777ae034c80
Freemano <freemano@MACs-MacBook-Pro.local>
2024-02-15 17:08:08 +0300

    main.js config

--- src/main.js
+++ src/main.js
@@ -1,40 +1,15 @@
-// import { createApp } from 'vue'
-// import App from './App.vue'
-// import router from './router'
-//
-// //bootstrap import
-// import 'bootstrap'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import VueSweetalert2  from "sweetalert2";
-// //bootstrap icon import
-// import 'bootstrap-icons/font/bootstrap-icons.css';
-// const app = createApp(App)
-//
-// app.use(router)
-//
-// app.mount('#app')
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import router from './router/index.js'
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        // Define your routes here
-    ]
-})
-
-const app = createApp(App)
-
-// Import Bootstrap CSS and JavaScript
+//bootstrap import
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
-// Import Bootstrap icons CSS
+import VueSweetalert2  from "sweetalert2";
+//bootstrap icon import
 import 'bootstrap-icons/font/bootstrap-icons.css';
+const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
-
